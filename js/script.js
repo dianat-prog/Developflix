ea2810cc7110ea2e77067844cffee52fe7fb6087
dianat-prog <dianatprogramadora@gmail.com>
2024-11-10 18:59:40 +0100

    Se añade cambios en script.js y styles.css

--- js/script.js
+++ js/script.js
@@ -1,50 +1,82 @@
 import peliculas  from './peliculas.js';
 
+const url=`https://image.tmdb.org/t/p/w500`;
 
-//Div Acción
-const divAccion =document.getElementById('genero-28');
 
-const peliAccion =peliculas.filter((genero) => genero.genre_ids.find((id)=> id===28));
-//console.log(peliAccion);
-
-peliAccion.forEach((peli)=>{
-    //creareamos un div  y dentro de ese div crearemos dos más: uno para la imagen de la pelicula y otro para el titulo
-    const divContenedor=document.createElement('div');
-    divContenedor.classList.add('contenedor'); //Le añadimos una clase para modificar el CSS
+//Función mostrar peli
+function mostraPeli(arrPelis,divSection){
+    arrPelis.forEach((peli)=>{
+        //creareamos un div  y dentro de ese div crearemos dos más: uno para la imagen de la pelicula y otro para el titulo
+        const divContenedor=document.createElement('div');
+        divContenedor.classList.add('contenedor'); //Le añadimos una clase para modificar el CSS
+        
+        const divImagen=document.createElement('div');
+        divImagen.classList.add('divimg'); //Le añadimos una clase para modificar el CSS
     
-    const divImagen=document.createElement('div');
-    divImagen.classList.add('divimg'); //Le añadimos una clase para modificar el CSS
+        const divTitulo=document.createElement('div');
+        divTitulo.classList.add('divtitulo'); //Le añadimos una clase para modificar el CSS
+    
+    
+       //Creamos un  objeto imagen
+        const imgPeli=document.createElement('img');
+        imgPeli.src=url +peli.poster_path;
 
-    const divTitulo=document.createElement('div');
-    divTitulo.classList.add('divtitulo'); //Le añadimos una clase para modificar el CSS
+        imgPeli.alt='imagen de la pelicula '+ peli.title;
+       // backdrop_path 
+    
+       //elemento h3 para el titulo de la pelicula
+        const tituloPeli=document.createElement('h3');
+        const linkTitulo=document.createElement('a');
+    
+        linkTitulo.textContent=peli.title;
+        linkTitulo.href ="#";
+        tituloPeli.appendChild(linkTitulo);
+        // tituloPeli.textContent=peli.title;
+    
+       //añadimos el elemento imagen en el div de imagen
+        divImagen.appendChild(imgPeli);
+    
+        //añadimos el elemento h3 en el div de titulo
+        divTitulo.appendChild(tituloPeli);
+    
+    
+        //Añadimos en el div contenedor los dos div : el de imagen y el de titulo
+        divContenedor.appendChild(divImagen);
+        divContenedor.appendChild(divTitulo);
+    
+        //por ultimo añadimos el div contenedor al genero-28
+        divSection.appendChild(divContenedor);
+    
+    })
+    
+}
 
-   
+
+//********************************************************* */
+//********************************************************* */
+//  Acción 28
+const divAccion =document.getElementById('genero-28');
+//Array con las peliculas de acción
+const peliAccion =peliculas.filter((genero) => genero.genre_ids.find((id)=> id===28)); 
+mostraPeli(peliAccion,divAccion);
 
 
-   //Creamos un  objeto imagen
-   const imgPeli=document.createElement('img');
-   imgPeli.src=peli.poster_path;
-   // backdrop_path 
 
-   //elemento h3 para el titulo de la pelicula
-   const tituloPeli=document.createElement('h3');
-   console.log(tituloPeli);
-   tituloPeli.textContent=peli.title;
-
-   //añadimos el elemento imagen en el div de imagen
-   divImagen.appendChild(imgPeli);
-
-    //añadimos el elemento h3 en el div de titulo
-    divTitulo.appendChild(tituloPeli);
+//********************************************************* */
+//********************************************************* */
+//thriller(53)
+const divThriller =document.getElementById('genero-53');
+//Array con las peliculas de thriller
+const peliThriller =peliculas.filter((genero) => genero.genre_ids.find((id)=> id===53)); 
+mostraPeli(peliThriller,divThriller);
 
 
-    //Añadimos en el div contenedor los dos div : el de imagen y el de titulo
-    divContenedor.appendChild(divImagen);
-    divContenedor.appendChild(divTitulo);
 
-    //por ultimo añadimos el div contenedor al genero-28
-    divAccion.appendChild(divContenedor);
 
-})
-
-//28 accion 53 Thriller  12 aventura
+//********************************************************* */
+//********************************************************* */
+//12 aventura
+const divAventura =document.getElementById('genero-12');
+//Array con las peliculas de aventura
+const peliAventura =peliculas.filter((genero) => genero.genre_ids.find((id)=> id===12)); 
+mostraPeli(peliAventura,divAventura);
